8a77b16d81dcf7272ae160fb79065b2f69bb1725
Zhixin Piao <1029945092@qq.com>
2018-09-09 14:23:18 +0800

    delete useless function

--- handler/gpu_handler.py
+++ handler/gpu_handler.py
@@ -4,16 +4,6 @@
 # @Email   : piaozhx@shanghaitech.edu.cn
 
 from handler.base_handler import BaseHandler
-from multiprocessing import Pool
-import json
-import os
-
-
-def get_useful_gpu_msg(node_id):
-    gpu_msg = os.popen('''ssh node%.2d '/home/piaozx/anaconda3/bin/python /home/piaozx/DockerMonitor/gpu_tools/get_gpu_msg.py' ''' % node_id).read()
-    gpu_msg = json.loads(gpu_msg)
-    return gpu_msg
-
 
 class GpuHandler(BaseHandler):
     def get(self):
