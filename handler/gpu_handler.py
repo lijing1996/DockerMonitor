ed53b082ab06f8340e9347886af61aee06960955
Yan ShiPeng <30871303+Rhyssiyan@users.noreply.github.com>
2018-11-29 12:49:07 +0800

    Update gpu_handler.py

--- handler/gpu_handler.py
+++ handler/gpu_handler.py
@@ -7,15 +7,26 @@ from handler.base_handler import BaseHandler
 
 class GpuHandler(BaseHandler):
     def get(self):
-        node_gpu_msg_list = self.db.get_node_msg_list()
-        self.render('../html/gpu.html', node_gpu_msg_list=node_gpu_msg_list, cur_user=self.get_current_user())
-
+        node_id = int(self.get_argument('id', default=-1))
+        if node_id == -1:
+            node_gpu_msg_list = self.db.get_node_msg_list()
+            self.render('../html/gpu.html', node_gpu_msg_list=node_gpu_msg_list, cur_user=self.get_current_user())
+        else:
+            node_gpu_msg_list = self.db.get_node_msg_list()
+            self.write(json.dumps(node_gpu_msg_list))
 
 
 class P40GpuHandler(BaseHandler):
     def get(self):
-        node_gpu_msg_list = self.db.get_p40_node_msg_list()
-        self.render('../html/p40_gpu.html', node_gpu_msg_list=node_gpu_msg_list, cur_user=self.get_current_user())
+        node_id = int(self.get_argument('id', default=-1))
+        if node_id == -1:
+            node_gpu_msg_list = self.db.get_p40_node_msg_list()
+            self.render('../html/p40_gpu.html', node_gpu_msg_list=node_gpu_msg_list, cur_user=self.get_current_user())
+        else:
+            node_gpu_msg_list = self.db.get_p40_node_msg_list()
+            self.write(json.dumps(node_gpu_msg_list))
+#         node_gpu_msg_list = self.db.get_p40_node_msg_list()
+#         self.render('../html/p40_gpu.html', node_gpu_msg_list=node_gpu_msg_list, cur_user=self.get_current_user())
 
 class CoursesGpuHandler(BaseHandler):
     def get(self):
